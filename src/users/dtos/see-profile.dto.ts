6dc943ef1cf3a5f2732bf2e1f6d1038cc584fb76
ruipaulocalei <ruipaulocalei0@gmail.com>
2022-05-12 20:48:51 +0100

    See profile dto

--- src/users/dtos/see-profile.dto.ts
+++ src/users/dtos/see-profile.dto.ts
@@ -1,10 +1,10 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { User } from "@prisma/client";
-import { OutputDto } from "src/common/dtos/output.dto";
-import { UserModel } from "src/models/users.model";
+import { Field, ObjectType } from '@nestjs/graphql';
+import { User } from 'generated/client';
+import { OutputDto } from 'src/common/dtos/output.dto';
+import { UserModel } from 'src/models/users.model';
 
 @ObjectType()
 export class SeeProfileOutput extends OutputDto {
-  @Field(type => UserModel, { nullable: true })
-  profile?: User
+  @Field((type) => UserModel, { nullable: true })
+  profile?: User;
 }
