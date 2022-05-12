813ed163767f9b05753e103285d0745384b51143
ruipaulocalei <ruipaulocalei0@gmail.com>
2022-05-12 20:50:13 +0100

    Configs to production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,16 +14,29 @@ import { PhotosModule } from './photos/photos.module';
 import { CommonModule } from './common/common.module';
 import { CommentsModule } from './comments/comments.module';
 import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env' : '.env.prod',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'production').required(),
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
       autoSchemaFile: true,
       context: ({ req, connection }) => {
-        const TOKEN_KEY = 'jwt-token'
-        return { token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY] }
-      }
+        const TOKEN_KEY = 'jwt-token';
+        return {
+          token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY],
+        };
+      },
     }),
     UsersModule,
     AuthModule,
@@ -30,4 +48,4 @@ import { MessagesModule } from './messages/messages.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
